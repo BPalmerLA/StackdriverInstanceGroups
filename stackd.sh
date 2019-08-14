afc43676d10af889bd1dd63a6727cabc2e4632eb
BPalmerLA <42742654+BPalmerLA@users.noreply.github.com>
2019-08-14 18:03:04 -0400

    Update stackd.sh

--- stackd.sh
+++ stackd.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install apache2
+sudo apt-get update \
+sudo apt-get install apache2 \
 
 ################ Installing Stackdriver Monitoriing Agent
-curl -sSO https://dl.google.com/cloudagents/install-monitoring-agent.sh
-sudo bash install-monitoring-agent.sh
+curl -sSO https://dl.google.com/cloudagents/install-monitoring-agent.sh \ 
+sudo bash install-monitoring-agent.sh \ 
 
 ############# Configuring Apache for the Monitoring Agent
-curl http://local-stackdriver-agent.stackdriver.com:80/server-status?auto
-(cd /opt/stackdriver/collectd/etc/collectd.d/ && sudo curl -O https://raw.githubusercontent.com/Stackdriver/stackdriver-agent-service-configs/master/etc/collectd.d/apache.conf)
+curl http://local-stackdriver-agent.stackdriver.com:80/server-status?auto \
+(cd /opt/stackdriver/collectd/etc/collectd.d/ && sudo curl -O https://raw.githubusercontent.com/Stackdriver/stackdriver-agent-service-configs/master/etc/collectd.d/apache.conf) \
 sudo service stackdriver-agent restart
